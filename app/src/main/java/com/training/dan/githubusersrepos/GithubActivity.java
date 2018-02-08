b5dfdc2e17ea1a8f170658e7fb4248656ea79c50
Danil Buhakov <tradinal@gmai.com>
2018-02-08 17:54:04 +0200

    Init commit

---
+++ app/src/main/java/com/training/dan/githubusersrepos/GithubActivity.java
@@ -0,0 +1,26 @@
+package com.training.dan.githubusersrepos;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.Menu;
+
+public class GithubActivity extends AppCompatActivity {
+    RecyclerView mRecyclerView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_github);
+
+        mRecyclerView = (RecyclerView) findViewById(R.id.recycler_view);
+        mRecyclerView.setLayoutManager(new LinearLayoutManager(this));
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.activity_github,menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+}
