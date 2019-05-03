cc86b5e53581886baf1ae5919a2a09facca5234b
Danil Buhokov <tradinal@gmail.com>
2019-05-03 15:13:06 +0300

    Added logging interceptor

--- app/src/main/java/com/training/dan/githubusersrepos/Retrofit/GithubRetrofit.java
+++ app/src/main/java/com/training/dan/githubusersrepos/Retrofit/GithubRetrofit.java
@@ -3,20 +3,32 @@ package com.training.dan.githubusersrepos.Retrofit;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 
+import okhttp3.OkHttpClient;
+import okhttp3.logging.HttpLoggingInterceptor;
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
 public class GithubRetrofit {
     private static final String BASE_URL = "https://api.github.com";
 
-    public static IGithub getApi(){
+    public static IGithub getApi() {
         Gson gson = new GsonBuilder()
                 .setLenient()
                 .create();
+
+        HttpLoggingInterceptor logging = new HttpLoggingInterceptor();
+        logging.setLevel(HttpLoggingInterceptor.Level.BODY);
+
+        OkHttpClient httpClient = new OkHttpClient.Builder()
+                .addInterceptor(logging)
+                .build();
+
         Retrofit retrofit = new Retrofit.Builder()
                 .baseUrl(BASE_URL)
                 .addConverterFactory(GsonConverterFactory.create(gson))
+                .client(httpClient)
                 .build();
+
         return retrofit.create(IGithub.class);
     }
 }
